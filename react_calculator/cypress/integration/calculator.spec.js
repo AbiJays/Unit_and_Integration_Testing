e92d4c4cd2a18cc514977b216a55f970327b845b
Abi Johnson <99146064+AbiJays@users.noreply.github.com>
2022-06-23 16:50:47 +0100

    test comment removal

--- react_calculator/cypress/integration/calculator.spec.js
+++ react_calculator/cypress/integration/calculator.spec.js
@@ -13,7 +13,7 @@ describe("Calculator", () => {
     cy.get('#number2').click();
     cy.get('.display').should('contain', '72')
     })
-    // Do the number buttons update the display of the running total?
+
      
   it('should update running total with correct outcome', () => {
     cy.get('#number7').click();
@@ -22,7 +22,7 @@ describe("Calculator", () => {
     cy.get('#operator-equals').click();
     cy.get('.display').should('contain', '9');
   })
-  // Do the arithmetical operations update the display with the result of the operation?
+
   
   it('should chain operations together', () => {
     cy.get('#number9').click();
@@ -33,7 +33,7 @@ describe("Calculator", () => {
     cy.get('#operator-equals').click();
     cy.get('.display').should('contain', '4');
     })
-    // Can multiple operations be chained together?
+
 
   it('should work with positive numbers', () => {
     cy.get('#number9').click();
@@ -44,7 +44,7 @@ describe("Calculator", () => {
     cy.get('#operator-equals').click();
     cy.get('.display').should('contain', '4');
     })
-    // Is the output as expected for a range of numbers? Write tests for positive, 
+
     
   it('should be able to output negative numbers', () => {
     cy.get('#number7').click();
@@ -53,7 +53,7 @@ describe("Calculator", () => {
     cy.get('#operator-equals').click();
     cy.get('.display').should('contain', '-2');
   })
-    // negative, 
+
     
   it('should work with decimal numbers ', () => {
     cy.get('#number5').click();
@@ -62,7 +62,7 @@ describe("Calculator", () => {
     cy.get('#operator-equals').click();
     cy.get('.display').should('contain', '2.5');
   })
-  //decimals
+
   
   it('should work with very large numbers ', () => {
     cy.get('#number6').click();
@@ -75,7 +75,7 @@ describe("Calculator", () => {
     cy.get('#operator-equals').click();
     cy.get('.display').should('contain', '92304');
   })
-  //very large numbers
+
   
   it('should give runtime error on divide by 0', () => {
     cy.get('#number6').click();
@@ -84,7 +84,5 @@ describe("Calculator", () => {
     cy.get('#operator-equals').click();
     cy.get('.display').should('contain', 'error');
   })
-    // What does the code do in exceptional circumstances? Specifically, if you divide by zero, what is the effect? Write a test to describe what you'd prefer to happen, and then correct the code to make that test pass (you will need to modify the Calculator model to meet this requirement).
-    // When 
 
 })
